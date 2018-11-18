83ab02040d5f9b2aeb200a2c6bb953ebfdc9bef9
Aly <sardina.aleigha@gmail.com>
2018-11-18 16:56:21 -0500

    Checkpoint -median

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,15 +1,15 @@
 class ShoppingCart:
-    def __init__(self):
+    def __init__(self, emp_discount=None):
         self._total = 0
         self._items = []
-        self._employee_discount = None
+        self._employee_discount = emp_discount
         
     @property    
     def total(self):
         return self._total
     @total.setter
     def total(self, new_total):
-        new_total = self._total
+        self._total = new_total
         return self.total
     @property
     def items(self):
@@ -25,14 +25,40 @@ class ShoppingCart:
     def employee_discont(self, discount):
         self._employee_discount = discount
         return self.employee_discount
-    #Next, we want to define an instance method called add_item that will add an item to our cart. It should take in the name of an item, its price and an optional quantity. The method should increase the shopping cart's total by the appropriate amount and return the new total for the shopping cart.
     
     def add_item(self, item_name, item_price, quantity=1):
-        
-        if grade_level in self._roster:
-            self._roster[grade_level].append(student_name)
-        else: 
-            self._roster[grade_level] = [student_name]
-        self.total += total
-    return self.total
-  
+        for item in list(range(quantity)):
+            self._items.append({'name': item_name, 'price':item_price})
+            self.total += item_price
+        return self.total
+    
+    def mean_item_price(self):
+        return (self.total/len(self.items))
+    
+    def median_item_price(self):
+        prices = [self._items(item, "price") for item in self.items]
+        prices.sort()
+        return self.find_median(prices)
+
+    def find_median(self, list_of_prices):
+        length = len(list_of_prices)
+        if (length%2 == 0):
+            index1 = int(length/2)
+            index2 = mid_one - 1
+            median = (list_of_prices[index1] + list_of_prices[index2])/2
+            return median
+        else:
+            index3 = int(length/2)  
+            return list_of_prices[index3]
+                          
+ #  mean_item_price and median_item_price  the mean is the average price per item and to find the median we must do three things:
+
+#First put all numbers in our list in ascending order (smallest to greatest)
+#Then check to see if there is an odd number of elements in our list. If so, the middle number is the median
+#Finally, if there is an even number of elements in the list, the median will be the average or mean of the two center elements (e.g. given the list [1,2,3,4] the elements 2 and 3 are the two center elements and the median would be (2 + 3)/2 or 2.5).
+    
+    
+    
+
+
+
